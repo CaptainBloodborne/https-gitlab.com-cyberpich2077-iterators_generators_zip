db2048bc8dc08b24768e8d9454c5ed0f7ce8ac2b
artem <vrail30@outlook.com>
2021-11-01 00:54:15 +0300

    Task corrected.

--- main.py
+++ main.py
@@ -3,16 +3,12 @@ from typing import Sequence
 
 def custom_zip(*args: Sequence):
     """
-    Generator function equivalent to built-in
-    function zip()
+    Generator function
     ---------
 
     :param args: any number of Sequence objects
     :return: Generator object with tuples
     """
-    for i in range(len(min(*args, key=len))):
-        yield tuple([j[i] for j in args])
-
-
-abc = custom_zip("ABC", ["a", "b", "c"], range(3))
-print(list(abc))
+    if args:
+        for i in range(len(min(*args, key=len))):
+            yield tuple([j[i] for j in args])
