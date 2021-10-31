c6e9e95fe4315b6d895ce1a076eac85350208557
artem <vrail30@outlook.com>
2021-11-01 00:45:31 +0300

    Task done.

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-def custom_zip(*args):
-    raise NotImplementedError("Implement me!")
+from typing import Sequence
+
+
+def custom_zip(*args: Sequence):
+    """
+    Generator function equivalent to built-in
+    function zip()
+    ---------
+
+    :param args: any number of Sequence objects
+    :return: Generator object with tuples
+    """
+    for i in range(len(min(*args, key=len))):
+        yield tuple([j[i] for j in args])
+
+
+abc = custom_zip("ABC", ["a", "b", "c"], range(3))
+print(list(abc))
